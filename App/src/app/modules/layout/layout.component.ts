3aae430d7eec697b38d02049b2b3ed322796b5f9
Falalatina <karolina40.gorska17@gmail.com>
2024-04-18 18:25:45 +0200

    theme done

--- App/src/app/modules/layout/layout.component.ts
+++ App/src/app/modules/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
