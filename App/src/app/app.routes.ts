3621c44e92db25e1ebe635eb6e669c96901325ea
Falalatina <karolina40.gorska17@gmail.com>
2024-04-18 17:08:58 +0200

    router

--- App/src/app/app.routes.ts
+++ App/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './modules/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'welcome',
+        pathMatch: 'full',
+      },
+      {
+        path: 'welcome',
+        loadComponent: () =>
+          import('./modules/welcome/welcome.component').then(
+            (c) => c.WelcomeComponent
+          ),
+      },
+    ],
+  },
+];
