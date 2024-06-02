2c7159855b39ef934d47c81fd8d80cc5b5eb18d2
Falalatina <karolina40.gorska17@gmail.com>
2024-06-02 21:50:17 +0200

    save

--- todo/src/app/app.routes.ts
+++ todo/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { TodoformComponent } from './components/todoform/todoform/todoform.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: TodoformComponent }];
---
+++ todo/src/app/components/todoform/todoform/todoform.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todoform',
+  standalone: true,
+  imports: [],
+  templateUrl: './todoform.component.html',
+  styleUrl: './todoform.component.scss',
+})
+export class TodoformComponent {
+  print() {
+    console.log('ua');
+  }
+}
