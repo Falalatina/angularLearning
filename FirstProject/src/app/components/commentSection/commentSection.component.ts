0a672f0d5634dc8fe2b56fb4092008933d64a130
Falalatina <karolina40.gorska17@gmail.com>
2024-10-30 10:40:31 +0100

    show comm

--- FirstProject/src/app/components/commentSection/commentSection.component.ts
+++ FirstProject/src/app/components/commentSection/commentSection.component.ts
@@ -6,4 +6,17 @@ import { Component, OnInit } from '@angular/core';
   selector: 'comment',
   templateUrl: './commentSection.component.html',
 })
-export class commentSection {}
+export class commentSection {
+  users = ['user1', 'user2', 'ser3'];
+  message = ['hi', 'papa', 'uwu'];
+  iteration = 0;
+  container: string[] = [];
+
+  showComment() {
+    this.iteration++;
+    this.container.push('');
+    if (this.iteration >= this.users.length) {
+      this.iteration = 0;
+    }
+  }
+}
