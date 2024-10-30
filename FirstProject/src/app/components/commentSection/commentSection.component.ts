acc1305d52b61bbcaf2b117f04434ab803b6cad3
Falalatina <karolina40.gorska17@gmail.com>
2024-10-30 11:08:35 +0100

    style

--- FirstProject/src/app/components/commentSection/commentSection.component.ts
+++ FirstProject/src/app/components/commentSection/commentSection.component.ts
@@ -5,18 +5,18 @@ import { Component, OnInit } from '@angular/core';
   imports: [],
   selector: 'comment',
   templateUrl: './commentSection.component.html',
+  styleUrl: './commentSection.component.scss',
 })
 export class commentSection {
   users = ['user1', 'user2', 'ser3'];
   message = ['hi', 'papa', 'uwu'];
   iteration = 0;
   container: string[] = [];
-
+  empty = ['Sorry there is no more comments :<'];
   showComment() {
     this.iteration++;
-    this.container.push('');
-    if (this.iteration >= this.users.length) {
-      this.iteration = 0;
+    if (this.iteration <= this.users.length + 1) {
+      this.container.push('');
     }
   }
 }
