84b26956eaf6dac63a0536b144cb38a099c42943
Falalatina <karolina40.gorska17@gmail.com>
2024-11-10 22:57:16 +0100

    router

--- FirstProject/src/app/app.routes.ts
+++ FirstProject/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { commentSection } from './components/commentSection/commentSection.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: commentSection,
+  },
+];
